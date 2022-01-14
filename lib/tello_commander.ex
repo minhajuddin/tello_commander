3cd7bd59d19696353e6648d5b98766e982477bfb
Minhajuddin Khaja <minhajuddin.k@gmail.com>
2022-01-13 21:50:19 -0500

    Add tello commander

--- lib/tello_commander.ex
+++ lib/tello_commander.ex
@@ -1,9 +1,28 @@
 defmodule TC do
-  @moduledoc """
-  TC keeps the contexts that define your domain
-  and business logic.
+  require Logger
+  @tello_port 8889
 
-  Contexts are also responsible for managing your data, regardless
-  if it comes from the database, an external API or others.
-  """
+  # connects to tello and starts the video stream
+  def start(ip) do
+    # connect to tello
+
+    # open a local udp socket
+    {:ok, sock} = :gen_udp.open(0, [:binary, {:active, true}])
+
+    # send "command" to get tello into sdk mode
+    send_command(sock, ip, "command")
+
+    # print some diagnostics
+    send_command(sock, ip, "battery?")
+  end
+
+  def send_command(sock, ip, data) do
+    :ok = :gen_udp.send(sock, ip, @tello_port, data)
+    receive do
+      msg ->
+        Logger.info message: msg, log_code: "received"
+    after 1000 ->
+        Logger.error "TIMEOUT"
+    end
+  end
 end
